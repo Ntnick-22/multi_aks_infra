7c598a7742dbedf0770e99e36540043521d4a532
Ntnick-22 <nyeinthunaing322@gmail.com>
2026-05-10 21:31:26 +0100

    add output description to be more meaningful

--- live/dev/00_rg/outputs.tf
+++ live/dev/00_rg/outputs.tf
@@ -1,7 +1,9 @@
 ﻿output "resource_group_name" {
-  value = module.rg.resource_group_name
+  description = "Name of the provisioned Azure resource group"
+  value       = module.rg.resource_group_name
 }
 
 output "resource_group_location" {
-  value = module.rg.resource_group_location
+  description = "Azure region where the resource group is deployed"
+  value       = module.rg.resource_group_location
 }
--- live/dev/01_networking/outputs.tf
+++ live/dev/01_networking/outputs.tf
@@ -1,7 +1,9 @@
 ﻿output "vnet_id" {
-  value = module.vnet.vnet_id
+  description = "Resource ID of the provisioned virtual network"
+  value       = module.vnet.vnet_id
 }
 
 output "subnet_ids" {
-  value = module.vnet.subnet_ids
+  description = "Map of subnet names to their resource IDs"
+  value       = module.vnet.subnet_ids
 }
